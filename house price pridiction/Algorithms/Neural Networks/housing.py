3267334719e2d96f2c5102a27a0ede7ec77e1e9d
DEEPAK0-2 <147275541+DEEPAK0-2@users.noreply.github.com>
2023-11-14 16:27:15 +0530

    Update housing.py

--- house price pridiction/Algorithms/Neural Networks/housing.py
+++ house price pridiction/Algorithms/Neural Networks/housing.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 28 13:27:25 2017
-
-@author: H.P. Asela
-"""
 
 import numpy as np
 import pandas as pd
@@ -11,8 +5,8 @@ from scipy.stats import skew
 from keras.models import Sequential
 from keras.layers import Dense
 
-train = pd.read_csv("D:/train.csv") # read train data
-test = pd.read_csv("D:/test.csv") # read test data
+train = pd.read_csv("D:/train.csv") 
+test = pd.read_csv("D:/test.csv") 
 
         
 outlier_idx = [4,11,13,20,46,66,70,167,178,185,199, 224,261, 309,313,318, 349,412,423,440,454,477,478, 523,540, 581,588,595,654,688, 691, 774, 798, 875, 898,926,970,987,1027,1109, 1169,1182,1239, 1256,1298,1324,1353,1359,1405,1442,1447]
@@ -21,7 +15,7 @@ all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'],test.loc[:,'MSSu
 to_delete = ['Alley','FireplaceQu','PoolQC','Fence','MiscFeature']
 all_data = all_data.drop(to_delete,axis=1)
 train["SalePrice"] = np.log1p(train["SalePrice"])
-#log transform skewed numeric features
+
 numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
 skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna())) #compute skewness
 skewed_feats = skewed_feats[skewed_feats > 0.75]
